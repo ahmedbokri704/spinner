2c636e6582b60620cd8325e996004c8188bf36cb
ahmedbokri <ahmedbokri25@gmail.com>
2020-04-17 10:23:19 +0100

    first commit

---
+++ src/data/movies-data.js
@@ -0,0 +1,63 @@
+const movies=[
+    {
+        img: "https://cdn-static.egybest.net/serve/movies/art-2834709917-x300.jpg",
+        name: "Ip Man 4: The Finale",
+        year: "2019",
+        star: 5,
+        duration: "01:45:12",
+        discription: "",
+        id:1
+
+    },
+    {
+        img: "https://cdn-static.egybest.net/serve/movies/art-2794910638-x300.jpg",
+        name: "StarDog and TurboCat",
+        year: "2019",
+        star: 4,
+        duration: "01:30:28",
+        discription: "",
+        id:2
+
+    },
+    {
+        img: "https://cdn-static.egybest.net/serve/movies/art-2866831890-x300.jpg",
+        name: "Fantasy Island",
+        year: "2014",
+        star: 2,
+        duration: "01:49:25",
+        discription: "",
+        id:3
+
+    },
+    {
+        img: "https://cdn-static.egybest.net/serve/movies/art-2892672302-x300.jpg",
+        name: "Money Heist: The Phenomenon",
+        year: "2020",
+        star: 4,
+        duration: "57:25",
+        discription: " ",
+        id:4
+
+    },
+    {
+        img: "https://cdn-static.egybest.net/serve/movies/art-1409110144-x300.jpg",
+        name: "The Legend of 420",
+        year: "2017",
+        star: 2,
+        duration: "01:26:54",
+        discription: "",
+        id:5
+
+    },
+    {
+        img: "https://cdn-static.egybest.net/serve/movies/art-2728623378-x300.jpg",
+        name: "Parasite",
+        year: "2019",
+        star: 5,
+        duration: "02:11:49",
+        discription: "",
+        id:5
+    }
+
+]
+export default movies
